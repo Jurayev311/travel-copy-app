dcd2c4579f9bd894c87f498595d91308b25e39ad
Javohir708 <joxauzking@gamil.com>
2025-03-21 19:17:07 +0500

    tekshirish

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,12 +9,20 @@ const poppins = Poppins({
 
 export const metadata = {
   title: "Travel",
-  description: "travel around the world",
+  description: "Travel around the world",
+  themeColor: "#1e40af",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* PWA uchun manifest.json */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1e40af" />
+        <link rel="apple-touch-icon" href="/assets/icon-192x192.png" />
+      </head>
       <body className={`${poppins.variable} antialiased`}>
         {children}
       </body>
